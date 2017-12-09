265cd69f24bba362dbe87931bf98e2d6f65c5eb0
Ryan Guo <ryanguo@Ryans-MacBook-Pro.local>
2017-12-09 11:28:14 +0800

    more practice

--- test.py
+++ test.py
@@ -1,1 +1,27 @@
-print("hello")
+def palindrome(str):
+    length = len(str)
+    for index in range(0, length // 2):
+        if str[index] != str[length-index-1]:
+            return False
+    return True
+
+def reverse(array):
+    size = len(array)
+    for index in range(0, int(size / 2)):
+        temp = array[index]
+        array[index] = array[size - index - 1]
+        array[size - index - 1] = temp
+    return array
+
+def is_prime(n):
+    for num in range(2, int(n/2)):
+        if n % num == 0:
+            return False
+    return True
+
+def gcd(x, y):
+    while y != 0:
+        temp = x
+        x = y
+        y = temp % y
+    return x

e6e62696eb4457038100a4300620d63b3e3f608e
Ryan Guo <ryanguo@Ryans-MacBook-Pro.local>
2018-02-09 20:39:11 +0800

    remove prerelease

--- prerelease/gen_yields.py
+++
@@ -1,5 +0,0 @@
-import random
-import os
-with open(os.path.join(os.path.join(os.getcwd(), "prerelease/yields.in")), "w") as myfile:
-    for i in range(140):
-        myfile.write(str(random.randint(3,10))+"\n")
